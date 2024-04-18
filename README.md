a74a84581afd0529da489d0a0c3a3962de5e49f2
Aditya Phasu <adityaphasu919@gmail.com>
2024-04-18 19:08:30 +0530

    Add files

---
+++ README.md
@@ -0,0 +1,41 @@
+# Firebase Authentication
+
+This is a simple application which uses Firebase Authentication to authenticate users and show their profile. It uses Firebase Email/Password and google sign-in methods to authenticate users. After signing in the user can update thier profile picture and display name.
+
+## Technologies Used
+
+![Firebase](https://img.shields.io/badge/Firebase-FFCA28.svg?style=for-the-badge&logo=Firebase&logoColor=black)
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black)
+![HTML5](https://img.shields.io/badge/HTML5-E34F26.svg?style=for-the-badge&logo=HTML5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6.svg?style=for-the-badge&logo=CSS3&logoColor=white)
+
+## How to install
+
+1. Clone the repository
+2. npm install
+
+```
+npm install
+```
+
+3. Setup Firebase and get the configuration
+
+```
+// You can get the configuration from Firebase Console after creating a new project
+
+const firebaseConfig = {
+  apiKey: // Your API Key,
+  authDomain: // Your Auth Domain,
+  projectId: // Your Project ID,
+  storageBucket: // Your Storage Bucket,
+  messagingSenderId: // Your Messaging Sender ID,
+  appId: // Your App ID
+```
+
+4. Start the development server
+
+```
+npm run dev
+```
+
+5. Open the browser and go to [http://localhost:5173](http://localhost:5173)
---
+++ index.js
@@ -0,0 +1,179 @@
+/* === Imports === */
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  updateProfile,
+} from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+/* === Firebase Setup === */
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+/* === UI === */
+
+/* == UI - Elements == */
+
+const viewLoggedOut = document.getElementById("logged-out-view");
+const viewLoggedIn = document.getElementById("logged-in-view");
+
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn");
+
+const emailInputEl = document.getElementById("email-input");
+const passwordInputEl = document.getElementById("password-input");
+
+const signInButtonEl = document.getElementById("sign-in-btn");
+const createAccountButtonEl = document.getElementById("create-account-btn");
+
+const signOutButtonEl = document.getElementById("sign-out-btn");
+
+const userProfilePictureEl = document.getElementById("user-profile-picture");
+const userGreetingEl = document.getElementById("user-greeting");
+
+const displayNameInputEl = document.getElementById("display-name-input");
+const photoURLInputEl = document.getElementById("photo-url-input");
+const updateProfileButtonEl = document.getElementById("update-profile-btn");
+
+/* == UI - Event Listeners == */
+
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle);
+
+signInButtonEl.addEventListener("click", authSignInWithEmail);
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail);
+signOutButtonEl.addEventListener("click", authSignOut);
+updateProfileButtonEl.addEventListener("click", authUpdateProfile);
+
+/* === Main Code === */
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    showLoggedInView();
+    showProfilePicture(userProfilePictureEl, user);
+    showUserGreeting(userGreetingEl, user);
+  } else {
+    showLoggedOutView();
+  }
+});
+
+/* === Functions === */
+
+/* = Functions - Firebase - Authentication = */
+
+function authSignInWithGoogle() {
+  signInWithPopup(auth, provider)
+    .then((result) => {})
+    .catch((error) => {
+      console.error(error.message);
+    });
+}
+
+function authSignInWithEmail() {
+  const email = emailInputEl.value;
+  const password = passwordInputEl.value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      clearAuthFields();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function authCreateAccountWithEmail() {
+  const email = emailInputEl.value;
+  const password = passwordInputEl.value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      clearAuthFields();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function authSignOut() {
+  signOut(auth)
+    .then(() => {
+      clearAuthFields();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function authUpdateProfile() {
+  updateProfile(auth.currentUser, {
+    displayName: displayNameInputEl.value,
+    photoURL: photoURLInputEl.value,
+  })
+    .then(() => {
+      console.log("Profile updated");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+/* == Functions - UI Functions == */
+
+function showLoggedOutView() {
+  hideView(viewLoggedIn);
+  showView(viewLoggedOut);
+}
+
+function showLoggedInView() {
+  hideView(viewLoggedOut);
+  showView(viewLoggedIn);
+}
+
+function showView(view) {
+  view.style.display = "flex";
+}
+
+function hideView(view) {
+  view.style.display = "none";
+}
+
+function clearInputField(field) {
+  field.value = "";
+}
+
+function clearAuthFields() {
+  clearInputField(emailInputEl);
+  clearInputField(passwordInputEl);
+}
+
+function showProfilePicture(imgElement, user) {
+  const photoURL = user.photoURL;
+
+  if (photoURL) {
+    imgElement.src = photoURL;
+  } else {
+    imgElement.src = "/assets/images/default-profile-picture.jpeg";
+  }
+}
+
+function showUserGreeting(element, user) {
+  const displayName = user.displayName;
+
+  if (displayName) {
+    element.textContent = `Hey ${displayName.split(" ")[0]}, how are you?`;
+  } else {
+    element.textContent = "Hey friend, how are you?";
+  }
+}
